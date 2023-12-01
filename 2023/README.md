d58cbab89c62246a77313af74f3696cebc8dcc75
Charlotte Mach <charlotte@verica.io>
2023-12-01 11:26:48 +0100

    Add 2023/1

---
+++ 2023/README.md
@@ -0,0 +1,10 @@
+# AoC 2023
+
+Attempting a different language every day again ... and again.
+
+Each day can be run by adding the input as <day>.txt into the <day> folder and then using
+```docker build -t <day> <day>/ && docker container run -it --rm <day>```
+
+E.g. ```docker build -t one one/ && docker container run -it --rm one```
+
+* 01 - Clojure
---
+++ 2023/one/one.clj
@@ -0,0 +1,41 @@
+(ns one.core
+  (:gen-class))
+(require '[clojure.string :as str])
+
+(defn filter-nums [ln]
+  (str/replace ln #"[^\d]" ""))
+
+(defn get-first [ln]
+  (re-find #"\d" (filter-nums ln)))
+
+(defn get-last [ln]
+  (re-find #"\d$" (filter-nums ln)))
+
+(defn a [data]
+  (reduce + 
+       (map 
+         #(Integer/parseInt (str (get-first %) (get-last %))) 
+         data)))
+
+(defn text-to-num [ln]
+   (str/replace (str/replace (str/replace 
+   (str/replace (str/replace (str/replace 
+   (str/replace (str/replace (str/replace ln 
+     #"one" "on1e") #"two" "tw2o") #"three" "thr3ee")
+     #"four" "fo4ur") #"five" "fi5ve") #"six" "si6x")
+     #"seven" "sev7en") #"eight" "ei8ght") #"nine" "ni9ne")
+)
+                
+(defn b [data]
+  (reduce + 
+       (map 
+         #(Integer/parseInt (str (get-first (text-to-num %)) (get-last (text-to-num %)))) 
+         data)))
+
+(defn -main
+  [& args]
+(with-open [r (clojure.java.io/reader "one.txt")]
+  (let [data (line-seq r)]
+     (prn (str "A: " (a data)))
+     (prn (str "B: " (b data)))
+)))
